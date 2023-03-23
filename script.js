5cb8d65cb121a265c94d0d335fc7f4a510e41743
Sarthak4561 <79055502+Sarthak4561@users.noreply.github.com>
2023-03-23 23:28:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let hmm = document.querySelector('form');
+hmm.onsubmit = okaay;
+let answer;
+
+
+function okaay(event){
+    event.preventDefault();
+    // console.log(event);
+    // console.log(document.querySelector('#number').value);
+    let guchi = new FormData(hmm);
+    // console.log(guchi.get('number'));
+    answer = guchi.get('number')
+}
+
+const show = document.getElementById('show');
+show.addEventListener('click',function(){
+    if(answer==1){
+        console.log(randomCard());
+        let paragraph = document.createElement('p')
+        paragraph.innerText = randomCard();
+        document.body.appendChild(paragraph);
+    }
+})
+
+
+const randomCard = function(){
+
+    const cards = ['Hearts','Diamond','Club','Spades'];
+    const numbers = ['Ace','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King']
+    const randomIndexCards = Math.floor (Math.random() * cards.length);
+    const randomIndexNumbers = Math.floor (Math.random() * numbers.length);
+
+    const randomCard = 'Your Cards is '+ numbers[randomIndexNumbers]+' of '+cards[randomIndexCards] ;
+
+    return randomCard;
+}
